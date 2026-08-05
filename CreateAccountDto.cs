00decd712ac326741597f2c99874b2d12fb16ade
Ntsako Lebogang Baloyi <89456863+NLB-Lebza@users.noreply.github.com>
2026-08-05 16:31:53 +0200

    Add files via upload

---
+++ CreateAccountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSystem.DTOs
+{
+    public class CreateCustomerDto
+    {
+        public string CustomerId { get; set; }
+
+        public string AccountType { get; set; }
+
+        public decimal InitialDeposit { get; set; }
+    }
+}
---
+++ WithdrawDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingSytem.DTOs
+{
+    public class WithdrawDto
+    {
+      public string FromAccountNumber { get; set; }
+
+        public string ToAccountNumber { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
