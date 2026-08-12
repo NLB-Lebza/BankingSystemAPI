3456089a4455204cea4343d2c7443b293895b42c
Lebogang <mrbaloyi02@gmail.com>
2026-08-12 22:38:54 +0200

    commit

---
+++ Repositories/AccountsRepo.cs
@@ -0,0 +1,35 @@
+﻿using BankingSystemAPI.Models;
+using BankingSystemAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingSystemAPI.Repositories;
+// This repository performs account and transaction database operations
+
+public class AccountsRepo : IAccountRepo
+{
+    private readonly BankingSystemAPI _context;
+
+    public AccountsRepo(BankingSystemAPI context)
+    {
+        this._context = context;
+    }
+
+    public async Task<BankAccount?> GetByAccountNumberAsync(string accountNumber)
+    {
+        return await _context.BankAccount
+            .Include(a => a.Customer)
+            .FirstOrDefaultAsync(a => a.AccountNumber == accountNumber);
+    }
+    public async Task<IEnumerable<Transactions>> GetTransactionsAsync(string accountNumber)
+    {
+        return await _context.Transactions
+            .Where(t => t.BankAccount.AccountNumber == accountNumber)
+            .OrderbyDescending(t => t.CreatedAt)
+            .ToListAsync();
+
+    }
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Repositories/IAccountRepo.cs
@@ -0,0 +1,11 @@
+﻿using BankingSystemAPI.Models;
+namespace BankingSystemAPI.Repositories
+{
+    public interface IAccountRepo
+    {
+       Task<BankAccount?> GetByAccountNumberAsync(string accountNumber);
+        Task<BankAccount>AddAsync(BankAccount account);
+        Task<IEnumerable<Transaction>>GetTransactionAsync(string accountNumber);
+        Task saveChangesAsync();
+    }
+}
